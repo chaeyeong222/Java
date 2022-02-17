082ee9f431e2955d98412d3a081b6ad701a204c8
chaeyeong222 <iky1505@naver.com>
2022-02-17 22:23:25 +0900

    init commit

---
+++ javaPro/src/days01/Ex01.java
@@ -0,0 +1,32 @@
+package days01;
+
+
+// Alt + Shift + J   -> 설명 다는 법
+
+/**
+ * @author 이채영
+ * @date 2022.02.15. pm 03:50:00
+ * @subject 주석처리 설명 및 단축키 설명
+ * @content
+ *    한줄 주석 처리 //
+ *    여러줄 주석 처리 /* ~ * /
+ */
+public class Ex01 {
+	
+	// ma Ctrl + Spcae.엔터
+
+	public static void main(String[] args) {
+		
+		//syso Ctrl+ Space
+		
+		System.out.println();
+	}
+	
+	//이클립스 주석 단축키 Ctrl+ / 누르면 블럭한 곳 다 주석 처리 가능
+	
+	// Ctrl + Shift+ L  => 단축키 목록
+	//이클립스에서 자주 사용하는 단축키 정리, 암기
+	
+	
+
+}
