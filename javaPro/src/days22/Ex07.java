ea9fc82279dc747c70ef306c163791984027c285
chaeyeong222 <iky1505@naver.com>
2022-03-28 00:56:04 +0900

    원하는 날짜의 달력 출력 예제

--- javaPro/src/days22/Ex07.java
+++ javaPro/src/days22/Ex07.java
@@ -6,31 +6,34 @@ import java.util.GregorianCalendar;
 public class Ex07 {
 
 	public static void main(String[] args) {
+		// [Calendar 클래스 사용 ]
 		int year = 2022;
 		int month = 3;
 		
-		//2010.5.1 무슨요일? 1일~7토
+		// 1. 2010.5.1    무슨 요일 ?    1(일) ~ 7(토)
 		int START_DAY_OF_WEEK = 0;
-		Calendar sDay = new GregorianCalendar(year,month-1,1);
-		START_DAY_OF_WEEK = sDay.get(Calendar.DAY_OF_WEEK);
+		Calendar sDay = new GregorianCalendar(year, month-1, 1);
+		START_DAY_OF_WEEK =  sDay.get(Calendar.DAY_OF_WEEK);	
 		
-		sDay.add(Calendar.DATE, -START_DAY_OF_WEEK+1);//2022.2.27 
+		// 2. 2010.5. ??  마지막날짜? 
+		int END_DAY = 0; 
+		Calendar eDay = new GregorianCalendar(year, month, 1);  // 2010.6.1
+		eDay.add(Calendar.DATE, -1);
+		END_DAY =  eDay.get(Calendar.DATE);
 		
 		
-		
-		//달력출력
-		System.out.printf("\t%d년%d월\n",year,month);
-		System.out.println("\t일\t월\t화\t수\t목\t금\t토\n");
-		for (int i = 1; i <= 42; i++) {
-			int date = sDay.get(Calendar.DATE);
-			System.out.printf(month==sDay.get(Calendar.MONTH)+1? "\t(%d)" : "\t%d",date);
-			sDay.add(Calendar.DATE, 1); //다음날짜
-			if(i%7==0) System.out.println();//개행
-			
-			
+		// 달력 출력
+		System.out.printf("\t%d년 %d월\n", year, month);
+		System.out.printf("일\t월\t화\t수\t목\t금\t토\n");
+		for (int i = 1; i < START_DAY_OF_WEEK; i++) {//1위치 잡기 위해 공백잡는 for문
+			System.out.print("\t");
+		}
+		for (int i = 1; i <= END_DAY; i++) {
+			System.out.printf("%d\t", i);
+			// 개행
+			if( ( i + START_DAY_OF_WEEK -1) % 7 ==0)  System.out.println();
 		}
 
+	} // main
 
-	}
-
-}
+} // class
--- javaPro/src/days22/Ex07_02.java
+++ javaPro/src/days22/Ex07_02.java
@@ -6,30 +6,28 @@ import java.util.GregorianCalendar;
 public class Ex07_02 {
 
 	public static void main(String[] args) {
-		int year = 2010;
-		int month = 5;
+		// [Calendar 클래스 사용 ]
+		int year = 2022;
+		int month = 3;
 		
-		//2010.5.1 무슨요일? 1일7토
+		// 1. 2010.5.1    무슨 요일 ?    1(일) ~ 7(토)
 		int START_DAY_OF_WEEK = 0;
-		Calendar sDay = new GregorianCalendar(year,month-1,1);
-		START_DAY_OF_WEEK = sDay.get(Calendar.DAY_OF_WEEK);
-		//2010.05 마지막날짜?
-		int END_DAY = 0;
-		Calendar eDay = new GregorianCalendar(year,month,1);
-		eDay.add(Calendar.DATE, -1);
-		END_DAY = eDay.get(Calendar.DATE);
+		Calendar sDay = new GregorianCalendar(year, month-1, 1);
+		START_DAY_OF_WEEK =  sDay.get(Calendar.DAY_OF_WEEK);	
 		
-		//달력출력
-		System.out.printf("\t%d년%d월\n",year,month);
-		System.out.println("일\t월\t화\t수\t목\t금\t토\n");
-		for (int i = 1; i < START_DAY_OF_WEEK; i++) {
-			System.out.println("\t");
+		sDay.add(Calendar.DATE, -START_DAY_OF_WEEK + 1);  // 2022.2.27
+		// 달력 출력
+		System.out.printf("\t%d년 %d월\n", year, month);
+		System.out.printf("\t일\t월\t화\t수\t목\t금\t토\n");
+		
+		for (int i = 1; i <=42; i++) {			
+			int date = sDay.get(Calendar.DATE);
+			System.out.printf(  month == sDay.get(Calendar.MONTH) +1   ?  "\t(%d)"  : "\t%d", date);
+			sDay.add(Calendar.DATE, 1); // 다음 날짜
+			if( i % 7 ==0 ) System.out.println();
 		}
-		for (int i = 1; i < END_DAY; i++) {
-			System.out.printf("%d\t",i);
-			if((i+START_DAY_OF_WEEK-1)%7==0) System.out.println();
-		}
+		
 
-	}
+	} // main
 
-}
+} // class
